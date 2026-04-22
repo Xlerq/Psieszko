ab7982eda5a1507ed42c1516e5b17998e7c7066f
Maciej Koryl <maciej.koryl@asseco.pl>
2026-04-22 15:51:12 +0200

    Wersja startowa aplikacji

---
+++ src/main/java/edu/prz/psieszko/PsieszkoApplication.java
@@ -0,0 +1,13 @@
+package edu.prz.psieszko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsieszkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsieszkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/prz/psieszko/PsieszkoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prz.psieszko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsieszkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
